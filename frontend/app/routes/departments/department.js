b8ff1e780991b7ebaa6bc1abb6a81f3e318fb6b4
Jørgen Solli <js@aplia.no>
2017-09-01 14:03:16 +0200

    Added favicon. Added controller to employee. Addd back-button to employee template. Other design changes.

--- frontend/app/routes/departments/department.js
+++ frontend/app/routes/departments/department.js
@@ -1,6 +1,6 @@
-import Ember from 'ember';
+import BaseRoute from '../base';
 
-export default Ember.Route.extend({
+export default BaseRoute.extend({
   queryParams: {
       q: { refreshModel: true },
   },
